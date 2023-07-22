ed43eb02387b198356217996357448127f0cc30b
Develogenics Full-Stack <seanmacangabhann@gmail.com>
2023-07-22 02:46:08 +0000

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,178 @@
+/*jslint browser:true, white:true */
+/*global Chart */
+
+
+var carouselElement = document.querySelector(".carousel");
+if (carouselElement) {
+  let firstImg = carouselElement.querySelectorAll("img")[0];
+  let arrowIcons = document.querySelectorAll(".wrapper i");
+  let isDragStart = false,
+    isDragging = false,
+    prevPageX,
+    prevScrollLeft,
+    positionDiff;
+
+  let showHideIcons = () => {
+    
+    let scrollWidth = carouselElement.scrollWidth - carouselElement.clientWidth; 
+    arrowIcons[0].style.display = carouselElement.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carouselElement.scrollLeft == scrollWidth ? "none" : "block";
+  };
+
+  arrowIcons.forEach((icon) => {
+    icon.addEventListener("click", () => {
+      let firstImgWidth = firstImg.clientWidth + 14; 
+    
+      carouselElement.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+      setTimeout(() => showHideIcons(), 60);
+    });
+  });
+
+  let autoSlide = () => {
+   
+    if (carouselElement.scrollLeft - (carouselElement.scrollWidth - carouselElement.clientWidth) > -1 || carouselElement.scrollLeft <= 0) return;
+    positionDiff = Math.abs(positionDiff); 
+    let firstImgWidth = firstImg.clientWidth + 14;
+    
+    let valDifference = firstImgWidth - positionDiff;
+    if (carouselElement.scrollLeft > prevScrollLeft) { 
+      return carouselElement.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+    }
+    
+    carouselElement.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+  };
+
+  let dragStart = (e) => {
+   
+    isDragStart = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = carouselElement.scrollLeft;
+  };
+
+  let dragging = (e) => {
+   
+    if (!isDragStart) return;
+    e.preventDefault();
+    isDragging = true;
+    carouselElement.classList.add("dragging");
+    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+    carouselElement.scrollLeft = prevScrollLeft - positionDiff;
+    showHideIcons();
+  };
+
+  let dragStop = () => {
+    isDragStart = false;
+    carouselElement.classList.remove("dragging");
+    if (!isDragging) return;
+    isDragging = false;
+    autoSlide();
+  };
+
+  carouselElement.addEventListener("mousedown", dragStart);
+  carouselElement.addEventListener("touchstart", dragStart);
+  document.addEventListener("mousemove", dragging);
+  carouselElement.addEventListener("touchmove", dragging);
+  document.addEventListener("mouseup", dragStop);
+  carouselElement.addEventListener("touchend", dragStop);
+}
+
+
+
+let quizData = [
+  {
+    question: "Can anyone learn archery",
+    choices: ["Yes", "No", "Maybe"],
+    correctAnswer: 1,
+  },
+  {
+    question: "Which planet is closest to the Sun?",
+    choices: ["Venus", "Mercury", "Mars"],
+    correctAnswer: 1,
+  },
+  {
+    question: "What is the largest mammal in the world?",
+    choices: ["Blue Whale", "Elephant", "Giraffe"],
+    correctAnswer: 0,
+  },
+];
+
+let questionElement = document.getElementById("question");
+let choicesElement = document.getElementById("choices");
+let submitButton = document.getElementById("submit-btn");
+let resultElement = document.getElementById("result");
+
+let currentQuestion = 0;
+let score = 0;
+
+function showQuestion() {
+  if (currentQuestion < quizData.length) {
+    let question = quizData[currentQuestion].question;
+    let choices = quizData[currentQuestion].choices;
+
+    questionElement.textContent = question;
+    choicesElement.innerHTML = "";
+
+    choices.forEach((choice, index) => {
+      let choiceElement = document.createElement("div");
+      choiceElement.className = "choice";
+
+      let radioInput = document.createElement("input");
+      radioInput.type = "radio";
+      radioInput.name = "quizChoice";
+      radioInput.value = index; 
+
+      let choiceLabel = document.createElement("label");
+      choiceLabel.textContent = choice;
+
+      choiceElement.appendChild(radioInput);
+      choiceElement.appendChild(choiceLabel);
+      choicesElement.appendChild(choiceElement);
+    });
+
+    submitButton.textContent = "Submit";
+  } else {
+    showResult();
+  }
+}
+
+function handleChoiceClick() {
+  const selectedRadio = document.querySelector('input[name="quizChoice"]:checked');
+  if (selectedRadio) {
+    let selectedIndex = parseInt(selectedRadio.value);
+    let correctAnswer = quizData[currentQuestion].correctAnswer;
+
+    if (selectedIndex === correctAnswer) {
+      score++;
+    }
+
+    currentQuestion++;
+    showQuestion();
+  }
+}
+
+function showResult() {
+  questionElement.textContent = "";
+  choicesElement.innerHTML = "";
+  resultElement.textContent = `You scored ${score} out of ${quizData.length} questions.`;
+  resultElement.style.display = "block";
+  submitButton.style.display = "none";
+}
+
+submitButton.addEventListener("click", handleChoiceClick);
+showQuestion();
+
+
+  let currentPath = window.location.pathname;
+
+  
+  let links = document.querySelectorAll(".navbar .nav-link");
+  links.forEach(link => {
+    if (link.getAttribute("href") === currentPath) {
+      link.classList.add('active-link');
+    }
+  });
+
+
+function hello(){ 
+alert("Welcome!  > > >   Click ok to be a boss!"); 
+}
